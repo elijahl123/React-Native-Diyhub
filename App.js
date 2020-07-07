5dea16768b67a4822d1459350f351063fc6ece9a
elijahl123 <61032860+elijahl123@users.noreply.github.com>
2020-07-06 22:03:53 -0700

    Final changes for today

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Icon } from 'react-native-vector-icons/';
 
 export default function App() {
   return (
