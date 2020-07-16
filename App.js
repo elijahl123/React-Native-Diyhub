e50505896b231f062e8ac7e4714c059f323a0e0c
Elijah <elijah.kane.1972@gmail.com>
2020-07-15 20:29:38 -0700

    Updating

--- App.js
+++ App.js
@@ -2,29 +2,99 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
-import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+function Search() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Search!</Text>
+        </View>
+    );
+}
+
+function Create() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Notifications!</Text>
+        </View>
+    );
+}
+
+function Discover() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Discover!</Text>
+        </View>
+    );
+}
+
+function Profile() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Profile!</Text>
+        </View>
+    );
+}
 
 const Tab = createMaterialBottomTabNavigator();
 
-export default function App() {
-  return (
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-  );
+function MyTabs() {
+    return (
+        <Tab.Navigator
+            initialRouteName="Feed"
+            activeColor="#FFF"
+            labelStyle={{ fontSize: 12 }}
+            style={{ backgroundColor: 'tomato' }}
+        >
+            <Tab.Screen
+                name="Search"
+                component={Search}
+                options={{
+                    tabBarLabel: 'Search',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="magnify" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Create"
+                component={Create}
+                options={{
+                    tabBarLabel: 'Create',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="plus-box" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Discover"
+                component={Discover}
+                options={{
+                    tabBarLabel: 'Discover',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="compass" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    tabBarLabel: 'Profile',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="account" color={color} size={26} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1B262C',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 50,
-  },
-});
+export default function App() {
+    return (
+        <NavigationContainer>
+            <MyTabs />
+        </NavigationContainer>
+    );}
